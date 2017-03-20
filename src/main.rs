53a2ef46b159fe65de6f104cf23dc6ab386ef6c7
Pascal Seitz <pascal.seitz@gmail.com>
2017-03-21 00:08:17 +0100

    yeash

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use piston::input::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 use rand::distributions::{IndependentSample, Range};
+use rand::{thread_rng, Rng};
 
 static WIDTH: u32 = 640;
 static HEIGHT: u32 = 480;
@@ -17,14 +18,63 @@ static HEIGHT: u32 = 480;
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
     rotation: f64,   // Rotation for the square.
-    world:Vec<Cell>
+    cells:Vec<Cell>
 
 }
 
 pub struct Cell {
     x:f64,
     y:f64,
-    color: [f32; 4]
+    dna: CellDNA
+}
+
+
+impl Cell {
+    fn new(x:f64, y:f64, color: [f32; 4]) -> Cell {
+        Cell {
+            x:x, y:y, dna: CellDNA::new(color)
+        }
+    }
+}
+
+pub struct CellDNA {
+    energy:      f32,
+    color:       [f32; 4],
+    speed:       f32,
+    health:      f32,
+    photosynth:  bool,
+    eats_food:   bool,
+    size:        f32,
+    energyUsage: f32
+}
+
+impl CellDNA {
+    fn new(color: [f32; 4]) -> CellDNA {
+        CellDNA {
+            energy:      pickf32(0, 100),
+            color:       color,
+            speed:       pickf32(0, 100),
+            health:      pickf32(0, 100),
+            photosynth:  pick(0, 100) < 10,
+            eats_food:   pick(0, 100) > 90,
+            size:        pickf32(0, 100),
+            energyUsage: pickf32(0, 100)
+        }
+    }
+}
+
+fn pick(a: i32, b: i32) -> i32 {
+    let between = Range::new(a, b);
+    let mut rng = rand::thread_rng();
+    between.ind_sample(&mut rng)
+}
+
+fn pickf32(a: i32, b: i32) -> f32 {
+    pick(a  * 100, b * 100)as f32  / 100.0
+}
+
+fn randColor() -> [f32; 4] {
+    [pickf32(0, 1), pickf32(0, 1), pickf32(0, 1), 1.0]
 }
 
 impl App {
@@ -36,10 +86,16 @@ impl App {
 
         let square = rectangle::square(0.0, 0.0, 50.0);
         let rotation = self.rotation;
-        let ref world = self.world;
+        
         let (x, y) = ((args.width / 2) as f64,
                       (args.height / 2) as f64);
 
+        {
+            let cell = rand::thread_rng().choose_mut(&mut self.cells).unwrap();
+            cell.dna.color = randColor();
+        }
+
+        let ref cells = self.cells;
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(GREEN, gl);
@@ -48,13 +104,18 @@ impl App {
                                        .rot_rad(rotation)
                                        .trans(-25.0, -25.0);
             
-            for cell in world {
+            for cell in cells {
                 // println!("{:?}", (i as u32%WIDTH) as  f64);
-                rectangle(cell.color,
+                rectangle(cell.dna.color,
                     [cell.x * 5.0, cell.y * 5.0, 5.0, 5.0],
                     c.transform, gl);
             }
 
+            // self.cells[0].dna.color = randColor();
+
+            // let cell = rand::thread_rng().choose_mut(&self.cells);
+            // cell.dna.color = randColor();
+            // let randColor = rand::thread_rng().choose(&cell.dna.color));
             // rectangle([1.0, 0.0, 0.0, 1.0], // red
             //           [0.0, 0.0, 100.0, 100.0],
             //           c.transform, gl);
@@ -70,16 +131,11 @@ impl App {
     }
 }
 
-fn pick(a: i32, b: i32) -> i32 {
-    let between = Range::new(a, b);
-    let mut rng = rand::thread_rng();
-    between.ind_sample(&mut rng)
-}
 
 enum TYPE {
-    NOTHING,
-    PLANT,
-    ANIMAL
+    NOTHING = 150,
+    PLANT = 198,
+    ANIMAL = 200
 }
 
 fn main() {
@@ -99,7 +155,7 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut world:Vec<Cell> = Vec::new();
+    let mut cells:Vec<Cell> = Vec::new();
 
     for x in 0..(WIDTH/5){
         for y in 0..(HEIGHT/5){
@@ -118,16 +174,16 @@ fn main() {
             };
 
             let num = pick(0, 200) as f32;
-            world.push(Cell{color: color, x: x as f64, y: y as f64});
+            cells.push(Cell::new(x as f64, y as f64, color));
         }
     }
 
-    // world.resize((WIDTH * HEIGHT)as usize, [0.0, 0.0, 1.0, 1.0]);
+    // cells.resize((WIDTH * HEIGHT)as usize, [0.0, 0.0, 1.0, 1.0]);
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0,
-        world
+        cells
     };
 
     let mut events = Events::new(EventSettings::new());
